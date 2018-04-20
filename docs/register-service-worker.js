464b5f693f54d1762ed3a13d95179813f36833f9
francois nicaise <whitetigle@gmail.com>
2018-04-20 12:04:50 +0200

    try remote address for service worker because... why not ;)

--- docs/register-service-worker.js
+++ docs/register-service-worker.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js')
+    navigator.serviceWorker.register('https://whitetigle.github.io/fable-pwa/service-worker.js')
     .then(function() {
         console.info('service worker succesfully registered');
     }).catch(function(e) {
